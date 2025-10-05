961b6acaba5c2076df38a9a1698d3d46923eede5
7b809 <ej818793@gmail.com>
2025-10-05 21:04:56 +0530

    none.

--- app.py
+++ app.py
@@ -67,6 +67,13 @@ def stream_video():
         "accept": "*/*",
         "accept-language": "en-US,en;q=0.9",
         "range": "bytes=0-",
+        "sec-ch-ua": '"Google Chrome";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "video",
+        "sec-fetch-mode": "no-cors",
+        "sec-fetch-site": "same-site",
+        "referer": "https://forcedcinema.net/",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
     }
     r = requests.get(video_url, headers=headers, stream=True)
